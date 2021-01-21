03d616045de03bc3cf67244650f8aa8be6d6c2b0
tranan2509 <tranan2509@gmail.com>
2021-01-21 17:39:39 +0700

    commit

---
+++ new_online_course_web/src/java/DAO/URL.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author TRAN VAN AN
+ */
+public class URL {
+//    public static String url = "/new_online_course_web";
+    public static String url="";
+}
